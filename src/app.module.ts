8961f2e1ece8adaa735d9506388003107479558b
Gubar Sergey <sergey.gubar187@gmail.com>
2020-01-03 14:36:27 +0200

    Add JWT auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigController } from './config/config.controller';
 import { SourcesController } from './sources/source.controller';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController, ConfigController, SourcesController],
   providers: [AppService],
 })
